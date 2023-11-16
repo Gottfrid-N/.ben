e609855705a410ebca5f0df16e455cc3eae6dbcc
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2023-11-16 09:17:21 +0100

    2023-11-16b: also still confused
    lexer maybe works now? im stuck on parameters :/
    im not sure how other types than strings should be implemented

--- metalbeard/bismuth/lexer.py
+++ metalbeard/bismuth/lexer.py
@@ -5,6 +5,49 @@ import metalbeard.bismuth as bismuth
 
 
 class BismuthLexer(benny.Lexer):
+    def parameter_lex(self):
+        output = []
+
+        while self.current_char is not benny.tokens.characters.GROUP_CLOSE:
+            while self.current_char is not benny.tokens.characters.SEPERATOR:
+                output.append(benny.tokens.STRING_OPEN)
+                output.append(self.string_lex())
+                output.append(benny.tokens.STRING_CLOSE)
+
+    def string_lex(self):
+        output = []
+
+        self.advance()
+        while self.current_char is not benny.tokens.characters.STRING:
+            output.append(benny.tokens.char(self.current_char))
+        self.advance()
+
+    def bismuth_lex(self):
+        output = []
+
+        while self.current_char is not None:
+            match self.current_char:
+                case benny.tokens.characters.SCOPE_OPEN:
+                    output.append(benny.tokens.SCOPE_OPEN)
+                case benny.tokens.characters.SCOPE_CLOSE:
+                    output.append(benny.tokens.SCOPE_CLOSE)
+                    output.append(metalbeard.tokens.BISMUTH_CLOSE)
+                    break
+                case benny.tokens.characters.STRING:
+                    output.append(benny.tokens.STRING_OPEN)
+                    output.append(self.string_lex())
+                    output.append(benny.tokens.STRING_CLOSE)
+
+                case metalbeard.tokens.characters.MACRO:
+                    output.append(metalbeard.tokens.MACRO_OPEN)
+                    output.append(benny.tokens.STRING_OPEN)
+                    output.append(self.string_lex())
+                    output.append(benny.tokens.STRING_CLOSE)
+
+            self.advance()
+        output.append(metalbeard.tokens.BISMUTH_CLOSE)
+        return output
+
     def lex(self):
         output = []
 
@@ -19,8 +62,8 @@ class BismuthLexer(benny.Lexer):
                     continue
                 case metalbeard.tokens.characters.BISMUTH:
                     output.append(metalbeard.tokens.BISMUTH_OPEN)
-                    match self.current_char:
-                        case benny.tokens.GROUP.character
-
+                    self.advance()
+                    output.append(self.bismuth_lex())
 
             self.advance()
+        return output
