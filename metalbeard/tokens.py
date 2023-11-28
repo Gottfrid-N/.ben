fb1e1ca9c162646e28e9af13e17c1529ec380dd2
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2023-11-28 11:26:19 +0100

    2023-11-27: Still confused oh no, this is bad

--- metalbeard/tokens.py
+++ metalbeard/tokens.py
@@ -33,8 +33,3 @@ def boolean_operator(operator_):
 SCOPE_OPEN = benny.Token("SCOPE", "OPEN")
 SCOPE_CLOSE = benny.Token("SCOPE", "CLOSE")
 SCOPE_SEPERATOR = benny.Token("SCOPE", "SEPERATOR")
-GROUP_OPEN = benny.Token("GROUP", "OPEN")
-GROUP_CLOSE = benny.Token("GROUP", "CLOSE")
-GROUP_SEPERATOR = benny.Token("GROUP", "SEPERATOR")
-METALBEARD_OPEN = benny.Token("METALBEARD", "OPEN")
-METALBEARD_CLOSE = benny.Token("METALBEARD", "CLOSE")
