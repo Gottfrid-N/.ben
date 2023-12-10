c96db59f260f26a99d0359de6a9dd9c1a9514010
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2023-12-10 16:07:47 +0100

    2023-12-10: try to refactor but bajs

--- benny/position.py
+++ benny/position.py
@@ -1,19 +1,14 @@
 class Position:
     def __init__(self):
-        self.char = -1
+        self.character = 0
         self.column = 0
-        self.line = 1
+        self.line = 0
 
     def increment(self):
-        self.char += 1
+        self.character += 1
         self.column += 1
 
     def increment_line(self):
+        self.increment()
         self.line += 1
-        self.column = 1
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        return f"<character: {self.char} || line: {self.line}, column: {self.column}>"
+        self.column = 0
--- metalbeard/lexer.py
+++ metalbeard/lexer.py
@@ -1,63 +1,21 @@
-import re
-
 import benny
-import metalbeard.tokens
-import metalbeard.macros
 
 
-class Lexer(benny.lexer.Lexer):
-    def value(self):
-        string = ""
-        while self.current_char != "\"":
-            string += self.current_char
-            self.advance()
-        return string
-
-    def operator(self):
-        match self.current_char:
-            case "=":
-                self.advance()
-                self.skip_whitespace()
-
-    def identifier(self, style_name, style, **kwargs):
-        identifier = ""
-        while re.match("\w", self.current_char):
-            identifier += self.current_char
-            self.advance()
-        if not style(identifier, **kwargs):
-            print(f"Invalid Identifier style in \"{identifier}\" at {self.position.__str__()}\n\n"
-                  f"Expected {style_name}")
-        return identifier
-
-    def assign_expression(self):
+def logic(input_: str):
+    pass
 
 
-    def call_expression(self):
+def lex(input_: str):
+    output = []
+    position = benny.Position()
+    while position.character <= len(input_):
+        current_character = input_[position.character]
+        match current_character:
+            case _ if current_character in benny.text.whitespace:
+                if current_character == "\n":
+                    position.increment_line()
 
-
-    def lex_expression(self):
-        match self.current_char:
-            case "%":
-                self.advance()
-                self.assign_expression("SCREAMING_SNAKE_CASE", benny.text.case.screaming_snake_case)
-            case "#":
-                self.advance()
-                self.expression("camelCase", benny.text.case.camel_case)
-            case "{":
-                pass
-
-    def lex_logic(self):
-        match self.current_char:
-            case "@":
-                self.advance()
-                self.lex_expression()
-            case _:
-                self.append_token(metalbeard.tokens.character(self.current_char))
 
 
 if __name__ == "__main__":
-    input_ = open("A:\\Projects\\.shhtml\\metalbeard\\input.metalbeard", "r")
-
-    lexer = Lexer(input_.read())
-    Lexer.lex(lexer)
-    print(lexer.tokens)
+    lex("12")
